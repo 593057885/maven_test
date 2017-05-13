eaf185d37c140123f8980e0d46990e2df7066d8a
593057885 <593057885@qq.com>
2017-05-13 11:00:27 +0800

    李炳华修改了文件内容

--- mavenweb Maven Webapp/src/main/resources/com/lbh/web/TestWeb.java
+++ mavenweb Maven Webapp/src/main/resources/com/lbh/web/TestWeb.java
@@ -3,5 +3,6 @@ package com.lbh.web;
 public class TestWeb {
 	public void test(){
 		System.out.println("hello boy");
+		System.out.println("来一波协同开发");
 	}
 }
---
+++ mavenweb Maven Webapp/src/main/resources/com/lbh/web/UserWeb.java
@@ -0,0 +1,7 @@
+package com.lbh.web;
+
+public class UserWeb {
+	public void doGet(){
+		System.out.println("新建的");
+	}
+}
